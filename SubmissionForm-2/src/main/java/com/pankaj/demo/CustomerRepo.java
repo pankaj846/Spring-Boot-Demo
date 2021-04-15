57a15ae5c32d7aa8fc51fd774201ae8465dcb639
Pankaj Singh <56686090+pankaj846@users.noreply.github.com>
2021-04-15 22:31:01 +0530

    Delete SubmissionForm-2 directory

--- SubmissionForm-2/src/main/java/com/pankaj/demo/CustomerRepo.java
+++
@@ -1,9 +0,0 @@
-package com.pankaj.demo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CustomerRepo extends JpaRepository<Customers, Integer> {
-// this interface is responsible to work with the customer class and the primary key will be the integer i.e customer ID
-
-}
